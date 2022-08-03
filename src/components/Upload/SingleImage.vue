a1313374b7082d5be20e470257d7183e447d562a
Ali raza <72508498+Aliraza2686@users.noreply.github.com>
2022-08-02 21:50:02 -0700

    Update SingleImage.vue
    added some divs around

--- src/components/Upload/SingleImage.vue
+++ src/components/Upload/SingleImage.vue
@@ -9,7 +9,7 @@
       drag
       action="https://httpbin.org/post"
     >
-      <i class="el-icon-upload" />
+      <div><i class="el-icon-upload" /></div>
       <div class="el-upload__text">
         将文件拖到此处，或<em>点击上传</em>
       </div>
